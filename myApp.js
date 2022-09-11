17dc1f0680fc2103169e6d5e0315c0905cb80463
BanDevLab <banegaspablopdb@gmail.com>
2022-09-11 16:23:22 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,16 +1,21 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
+//sirve archivo html
 app.get("/",
        function(req,res){
          //res.send("Hello Express");
          let path=__dirname+"/views/index.html";
          res.sendFile(path);
        });
+//servir recursos static
 app.use("/public",
        express.static(__dirname+"/public"));
-
-
+//servimos Json
+app.get("/json",
+       function(req,res){
+              res.json({"message":"Hello json"});
+       });
 
 
 
